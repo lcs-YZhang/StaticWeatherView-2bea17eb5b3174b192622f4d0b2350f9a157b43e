944964a0dfb05ddc69badc3276ffea74020c4276
Steven Zhang <YZhang@lcs.on.ca>
2021-10-06 10:08:34 -0400

    Initial Commit

---
+++ StaticWeatherView/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  StaticWeatherView
+//
+//  Created by Russell Gordon on 2021-10-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        // Use layers to show background colour
+        // See page 79 of SwiftUI Views Quick Start
+        // https://www.bigmountainstudio.com/free-swiftui-book
+        ZStack {
+            
+            // First layer
+            // For gradient example, see: https://www.hackingwithswift.com/quick-start/swiftui/how-to-render-a-gradient
+            LinearGradient(colors: [.blue, .white],
+                           startPoint: .top,
+                           endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            
+            // Second layer
+            VStack {
+                
+                TitleBarView()
+                    .padding(.bottom, 50)
+                                
+                VStack(spacing: 50) {
+                    
+                    TemperatureAndWindView()
+                
+                    Past24HoursView()
+                    
+                }
+                
+                Spacer()
+                
+                // NOTE: Image must be placed in the Assets folder
+                Image("logo-canada")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxHeight: 40)
+                    
+
+            }
+            .foregroundColor(.white)
+
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
